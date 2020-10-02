6928bec58391878904d4edf3a1ef527e5da15d1d
gerbasi117 <61759483+gerbasi117@users.noreply.github.com>
2020-10-01 23:54:09 -0700

    added an attr_accessor, and initialized attributes.

--- lib/input_info.rb
+++ lib/input_info.rb
@@ -1,7 +1,15 @@
 class Character_info
+    attr_accessor :name, :status, :species, :type, :gender, :origin
     @@all=[]
    
-    def initialize
+    def initialize(name: nil, input: nil, status: nil, species: nil, type: nil, gender: nil, origin: nil)
+        @name = name
+        @input = input
+        @status = status
+        @species = species
+        @type = type
+        @gender = gender 
+        @origin = origin    
         @@all << self
     end
 
