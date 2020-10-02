e805161f159403de83fd6a077e7bf9ee025bffee
gerbasi117 <61759483+gerbasi117@users.noreply.github.com>
2020-10-01 21:29:18 -0700

    Added a menu method, that has a user interface to initiate the pull of character_info.

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,3 +1,12 @@
 class CLI
-    
+    def menu
+        puts ""
+        puts "EnRicklopedia"
+        puts "_____________________"
+        puts ""
+        puts "Enter Character name to get information or 'exit' to exit"
+        puts ""
+        @character_info = gets.strip.downcase
+        API.get_name(@character_info)       
+    end    
 end
